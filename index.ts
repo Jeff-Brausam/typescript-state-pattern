ca35dec536af83e7698cda01f289bd3b0af16bd0
jeff brausam <jeffbrausam@gmail.com>
2021-06-04 22:40:10 -0700

    state flow core pattern. Create, Update, Delete, Clear, Select.

---
+++ index.ts
@@ -0,0 +1,173 @@
+/* 
+    Objects are your nouns of programming. For the clearest communication you must 
+    be able to describe things. Who is your user, what are some of their features and characteristics 
+    define the entity or object you are working with. 
+*/
+interface BaseEntity {
+  id: string | null;
+}
+
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+interface User extends BaseEntity {
+  email: string;
+}
+
+const Allysa: User = {
+  id: "1",
+  email: "AlyssaG@gmail.com",
+};
+
+const James: User = {
+  id: "2",
+  email: "JamesB@yahoo.com",
+};
+
+const Users: User[] = [Allysa, James];
+
+interface UsersState {
+  users: User[];
+  CurrentUser: User;
+}
+
+const newUser: User = {
+  id: null,
+  email: "",
+};
+
+const initialUserState: UsersState = {
+  users: Users,
+  CurrentUser: newUser,
+};
+
+interface AppState {
+  usersState: UsersState[];
+}
+
+// Create a store
+class UsersStore {
+  reducer;
+  state: UsersState;
+
+  constructor(state: UsersState, reducer) {
+    this.state = state;
+    this.reducer = reducer;
+  }
+
+  getState() {
+    return this.state;
+  }
+  select(key: string) {
+    return this.state[key];
+  }
+  dispatch(action: Action) {
+    this.state = this.reducer(this.state, action);
+  }
+}
+
+/* 
+  Functions and actions are your verbs of programming. What are those things that you 
+  have defined earlier doing? How do you want to interact with this collection of objects
+  or state, and possibly affect the application.  
+*/
+
+// Create a reducer
+const USER_LOAD = "[User] Load";
+const USER_CREATE = "[User] Create";
+const USER_UPDATE = "[User] Update";
+const USER_DELETE = "[User] Delete";
+const USER_SELECT = "[User] Select";
+const USER_CLEAR = "[User] Clear";
+
+const loadUser = (state, users): UsersState => {
+  return {
+    users,
+    CurrentUser: state.CurrentUser,
+  };
+};
+const selectUser = (state, user): UsersState => {
+  return {
+    users: state.users,
+    CurrentUser: user.payload,
+  };
+};
+const clearUser = (state): UsersState => {
+  return {
+    users: state.users,
+    CurrentUser: null,
+  };
+};
+const createUser = (state, user): UsersState => {
+  return {
+    users: [...state.users, user.payload],
+    CurrentUser: state.CurrentUser,
+  };
+};
+const updateUser = (state, user): UsersState => {
+  return {
+    users: state.users.map((u) =>
+      u.id === user.payload.id ? (<any>Object).assign({}, user.payload) : u
+    ),
+    CurrentUser: state.CurrentUser,
+  };
+};
+const deleteUser = (state, user): UsersState => {
+  return {
+    users: state.users.filter((u) => u.id !== user.payload.id),
+    CurrentUser: state.CurrentUser,
+  };
+};
+
+const usersReducer = (state: UsersState = initialUserState, action: Action) => {
+  switch (action.type) {
+    case USER_LOAD: {
+      return loadUser(state, action);
+    }
+    case USER_SELECT: {
+      return selectUser(state, action);
+    }
+    case USER_CLEAR: {
+      return clearUser(state);
+    }
+    case USER_CREATE: {
+      return createUser(state, action);
+    }
+    case USER_UPDATE: {
+      return updateUser(state, action);
+    }
+    case USER_DELETE: {
+      return deleteUser(state, action);
+    }
+    default:
+      return state;
+  }
+};
+
+const Jane: User = {
+  id: "123",
+  email: "janewoah@hotmail.com",
+};
+
+const updatedJane: User = {
+  id: "123",
+  email: "janesnewemail@yahoo.com",
+};
+
+// Put it all together!
+const userStore = new UsersStore(initialUserState, usersReducer);
+userStore.getState();
+userStore.dispatch({ type: USER_CREATE, payload: Jane });
+console.log("[User] created! Current State: ", userStore.getState());
+userStore.dispatch({ type: USER_UPDATE, payload: updatedJane });
+console.log("[User] updated! Current State: ", userStore.getState());
+userStore.dispatch({ type: USER_DELETE, payload: Jane });
+console.log("[User] deleted! Current State: ", userStore.getState());
+userStore.dispatch({ type: USER_CLEAR, payload: James });
+console.log("[User] cleared! Current State: ", userStore.getState());
+userStore.dispatch({ type: USER_SELECT, payload: James });
+console.log("[User] selected! Current State: ", userStore.getState());
+userStore.dispatch({ type: USER_CLEAR, payload: James });
+console.log("[User] cleared! Current State: ", userStore.getState());
